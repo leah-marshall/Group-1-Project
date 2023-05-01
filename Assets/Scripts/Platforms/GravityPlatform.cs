2588bf817526283950b4561854631f4d495c30ba
quimmn <114498524+quimmn@users.noreply.github.com>
2023-05-01 15:16:02 +0100

    Gravity Platforms

--- Assets/Scripts/Platforms/GravityPlatform.cs
+++ Assets/Scripts/Platforms/GravityPlatform.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GravityPlatform : MonoBehaviour
 {
+    private Rigidbody playerBody;
+    [SerializeField] private Vector3 GravityDirection;
+    private bool inGravityArea = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GameObject.Find("Player").GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (inGravityArea){
+            playerBody.AddForce(GravityDirection);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            inGravityArea = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            inGravityArea = false;
+        }
     }
 }
