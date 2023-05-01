f53c53d74549cd5924296f830cfe6423f6dfbfd3
quimmn <114498524+quimmn@users.noreply.github.com>
2023-05-01 15:29:26 +0100

    Revert "Gravity Platforms"
    This reverts commit 2588bf817526283950b4561854631f4d495c30ba.

--- Assets/Scripts/Platforms/GravityPlatform.cs
+++ Assets/Scripts/Platforms/GravityPlatform.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class GravityPlatform : MonoBehaviour
 {
-    private Rigidbody playerBody;
-    [SerializeField] private Vector3 GravityDirection;
-    private bool inGravityArea = false;
     // Start is called before the first frame update
     void Start()
     {
-        playerBody = GameObject.Find("Player").GetComponent<Rigidbody>();
-
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (inGravityArea){
-            playerBody.AddForce(GravityDirection);
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Player")
-        {
-            inGravityArea = true;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.name == "Player")
-        {
-            inGravityArea = false;
-        }
+        
     }
 }
