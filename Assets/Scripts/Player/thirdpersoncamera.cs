c02e17e4028cfe0e2de16c56bd3d99f342348234
quimmn <114498524+quimmn@users.noreply.github.com>
2023-05-01 12:16:27 +0100

    Camera roll now orbits around player

--- Assets/Scripts/Player/thirdpersoncamera.cs
+++ Assets/Scripts/Player/thirdpersoncamera.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class thirdpersoncamera : MonoBehaviour
 {
-    private Transform playerSphere;
+    private Transform playerRoll;
     private ballcontroller playerController;
     private float currentRoll; // stores starting y position of the mouse, then tracks current position via change in y position
 
     // Start is called before the first frame update
     void Start()
     {
-        playerSphere = GameObject.Find("Player").GetComponent<Transform>(); // using separate script to control local rotation of camera (do not want to roll player's body)
-        playerController = playerSphere.GetComponent<ballcontroller>();
+        playerRoll = GameObject.Find("Rotator").GetComponent<Transform>(); // using separate script to control local rotation of camera (do not want to roll player's body)
+        playerController = playerRoll.parent.GetComponent<ballcontroller>();
         currentRoll = Input.mousePosition.y;
     }
 
@@ -23,7 +23,7 @@ public class thirdpersoncamera : MonoBehaviour
         float deltaY = Input.GetAxis("Mouse Y");
         currentRoll += -deltaY * playerController.sensitivity; // rather than setting camera roll directly with mouse y pos, track change in positon and multiply by adjustable sensitivity so that cursor can be locked
         currentRoll = Mathf.Clamp(currentRoll, -90, 90);
-        camRoll.eulerAngles = new Vector3(currentRoll, playerSphere.rotation.y, playerSphere.rotation.z); // set camera roll to keep player's y and z rotation, set rotation about x to mouse y pos
-        transform.localRotation = camRoll; // only change local rotation to prevent yaw overwrite
+        camRoll.eulerAngles = new Vector3(currentRoll, playerRoll.rotation.y, playerRoll.rotation.z); // set camera roll to keep player's y and z rotation, set rotation about x to mouse y pos
+        playerRoll.localRotation = camRoll; // only change local rotation to prevent yaw overwrite
     }
 }
