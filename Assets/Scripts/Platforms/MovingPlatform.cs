4c08b295854bfe5ff8b431ee806ec35c2b7eb774
leah-marshall <114776361+leah-marshall@users.noreply.github.com>
2023-05-17 17:52:46 +0100

    Update MovingPlatform.cs

--- Assets/Scripts/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -7,6 +7,9 @@ public class MovingPlatform : MonoBehaviour
     public GameObject[] points;
     [SerializeField] private float speed = 2f;
     int current_destination = 0;
+    [SerializeField] private const float WAIT_TIME = 2f;
+    private float waited_for = 0f;
+    private bool is_waiting = false;
 
 
     // Start is called before the first frame update
@@ -20,6 +23,11 @@ public class MovingPlatform : MonoBehaviour
     {
         if(Vector3.Distance(transform.position, points[current_destination].transform.position) < 0.5f)
         {
+            if(!is_waiting)
+            {
+                waited_for = 0f;
+                is_waiting = true;
+
                 if (current_destination < points.Length - 1)
                 {
                     current_destination++;
@@ -27,9 +35,23 @@ public class MovingPlatform : MonoBehaviour
                 else
                 {
                     current_destination = 0;
-                }   
-        }
+                }
+            }
         
-        transform.position = Vector3.MoveTowards(transform.position, points[current_destination].transform.position, speed * Time.deltaTime);
+        }
+
+        if(is_waiting)
+        {
+            waited_for += Time.deltaTime;
+            if (waited_for >= WAIT_TIME)
+            {
+                is_waiting = false;
+            }
+        }
+
+        if (!is_waiting)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current_destination].transform.position, speed * Time.deltaTime);
+        }
     }
 }
