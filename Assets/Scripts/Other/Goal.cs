a2f7b717fea7314360da4779bec9ccb70289210b
quimmn <114498524+quimmn@users.noreply.github.com>
2023-05-29 16:36:58 +0100

    Completed scene transitions

--- Assets/Scripts/Other/Goal.cs
+++ Assets/Scripts/Other/Goal.cs
@@ -26,6 +26,8 @@ public class Goal : MonoBehaviour
             music.Stop();
             music.PlayOneShot(goalClip);
             timeAnimation.Play("timeCentered");
+            timeAnimation.Play("timeCentered2");
+            timeAnimation.Play("timeCentered3");
             Vector3 velocityRef = Vector3.zero; // referenced unity docs https://docs.unity3d.com/ScriptReference/Vector3.SmoothDamp.html + scriptkid's comment https://forum.unity.com/threads/stopping-rigidbody-on-a-dime.263743/
             playerBody.velocity = Vector3.SmoothDamp(playerBody.velocity, new Vector3(0, playerBody.velocity.y, 0), ref velocityRef, 0.1f); 
             player.movementEnabled = false;
