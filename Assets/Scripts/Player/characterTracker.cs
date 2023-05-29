d94f1148219c3014689baf2ec856622f17df95a2
quimmn <114498524+quimmn@users.noreply.github.com>
2023-05-29 15:01:56 +0100

    Character instantiation fixed

--- Assets/Scripts/Player/characterTracker.cs
+++ Assets/Scripts/Player/characterTracker.cs
@@ -10,6 +10,13 @@ public class characterTracker : MonoBehaviour
     [SerializeField] private GameObject[] animalPrefabs;
     private Transform player;
     private ballcontroller playercontroller;
+    private int CAT = 0;
+    private int PUFF = 1;
+    private int OCTO = 2;
+    private int BIRD = 3;
+    private int BEAR = 4;
+    private int WHALE = 5;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -18,7 +25,28 @@ public class characterTracker : MonoBehaviour
                     Time.timeScale = 0f;
                     player = GameObject.Find("Player").GetComponent<Transform>();
                     playercontroller = GameObject.Find("Player").GetComponent<ballcontroller>();
-                    animalModel = Instantiate(animalPrefabs[animalToInstance], new Vector3(8, 4, -11.25f), Quaternion.identity);
+                    Vector3 animalPos = new Vector3(8, 4, -11.25f);
+                    Quaternion animalRotation = Quaternion.identity;
+                    if (animalToInstance == CAT){
+                        animalPos = new Vector3(8, 5.3f, -11.25f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == PUFF){
+                        animalPos = new Vector3(8, 5.3f, -11.25f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == OCTO){
+                        animalPos = new Vector3(8, 5.3f, -11.25f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == BIRD){
+                        animalPos = new Vector3(8, 5.3f, -11.25f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == BEAR){
+                        animalPos = new Vector3(8, 5.3f, -11.3f);
+                        animalRotation.eulerAngles = new Vector3(-90, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == WHALE){
+                        animalPos = new Vector3(8, 5.3f, -11.25f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    }
+                    animalModel = Instantiate(animalPrefabs[animalToInstance], animalPos, animalRotation); // 8, 4, -11.25
                     animalModel.transform.parent = player;
                     playercontroller.animator = GameObject.FindGameObjectsWithTag("Animations")[0].GetComponent<Animator>();
                     Time.timeScale = 1f;
@@ -26,7 +54,28 @@ public class characterTracker : MonoBehaviour
                     Time.timeScale = 0f;
                     player = GameObject.Find("Player").GetComponent<Transform>();
                     playercontroller = GameObject.Find("Player").GetComponent<ballcontroller>();
-                    animalModel = Instantiate(animalPrefabs[animalToInstance], new Vector3(0, 70, -90f), Quaternion.identity);
+                    Vector3 animalPos = new Vector3(8, 4, -11.25f);
+                    Quaternion animalRotation = Quaternion.identity;
+                    if (animalToInstance == CAT){
+                        animalPos = new Vector3(0, 69.3f, -90f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == PUFF){
+                        animalPos = new Vector3(0, 69.3f, -90f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == OCTO){
+                        animalPos = new Vector3(0, 69.3f, -90f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == BIRD){
+                        animalPos = new Vector3(0, 69.3f, -90f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == BEAR){
+                        animalPos = new Vector3(0, 69.3f, -90.05f);
+                        animalRotation.eulerAngles = new Vector3(-90, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == WHALE){
+                        animalPos = new Vector3(0, 69.3f, -90f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    }
+                    animalModel = Instantiate(animalPrefabs[animalToInstance], animalPos, animalRotation); 
                     playercontroller.animator = GameObject.FindGameObjectsWithTag("Animations")[0].GetComponent<Animator>();
                     animalModel.transform.parent = player;
                     Time.timeScale = 1f;
@@ -34,7 +83,28 @@ public class characterTracker : MonoBehaviour
                     Time.timeScale = 0f;
                     player = GameObject.Find("Player").GetComponent<Transform>();
                     playercontroller = GameObject.Find("Player").GetComponent<ballcontroller>();
-                    animalModel = Instantiate(animalPrefabs[animalToInstance], new Vector3(0, 500, -45f), Quaternion.identity);
+                    Vector3 animalPos = new Vector3(8, 4, -11.25f);
+                    Quaternion animalRotation = Quaternion.identity;
+                    if (animalToInstance == CAT){
+                        animalPos = new Vector3(0, 499.3f, -45f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == PUFF){
+                        animalPos = new Vector3(0, 499.3f, -45f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == OCTO){
+                        animalPos = new Vector3(0, 499.3f, -45f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == BIRD){
+                        animalPos = new Vector3(0, 499.3f, -45f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == BEAR){
+                        animalPos = new Vector3(0, 499.3f, -45.05f);
+                        animalRotation.eulerAngles = new Vector3(-90, 0, animalRotation.eulerAngles.z);
+                    } else if (animalToInstance == WHALE){
+                        animalPos = new Vector3(0, 499.3f, -45f);
+                        animalRotation.eulerAngles = new Vector3(animalRotation.eulerAngles.x, 90, animalRotation.eulerAngles.z);
+                    }
+                    animalModel = Instantiate(animalPrefabs[animalToInstance], animalPos, animalRotation); 
                     playercontroller.animator = GameObject.FindGameObjectsWithTag("Animations")[0].GetComponent<Animator>();
                     animalModel.transform.parent = player;
                     Time.timeScale = 1f;
